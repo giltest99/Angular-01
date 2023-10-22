8451eb94f53c5e7d529e0d9beb7c65ef63058fdc
Gilles <sellig.blater@free.fr>
2023-10-22 21:33:02 +0200

    Add not-found component. Update app-routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { KonkatComponent } from './konkat/konkat.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
   { path: 'konkat', component: KonkatComponent, title: 'Konkat' },
+  { path: '**', component: NotFoundComponent, title: 'Oups...' },
 ];
 
 @NgModule({
