74abf4713eeac93aabcfe39df5c49357e82f76d7
Gilles <sellig.blater@free.fr>
2023-10-22 21:28:30 +0200

    Routes home and konkat ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { KonkatComponent } from './konkat/konkat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'konkat', component: KonkatComponent, title: 'Konkat' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
